5d0cb79e82ea4a92c33e8cc08f35829f057304b3
Jon Low <lowjonc@gmail.com>
2017-10-18 15:15:59 -0600

    taking a break

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,26 +1,78 @@
 'use strict';
 
+
 console.log('build-it-visible is running');
 
 const appRoot = document.getElementById('app')
 
-let buttonText = 'Show Details'
-let textVis = true
 
-const theGreatClick = () => {
-  textVis ? textVis = false : textVis = true
-  buttonText == 'Show Details' ? buttonText = 'Hide Details' : buttonText = 'Show Details'
-  renderApp()
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this)
+    this.state = {
+      visibility: false
+    }
+  }
+
+  handleToggleVisibility(){
+    this.setState((prevState) =>{
+      return {
+        visibility: !prevState.visibility
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? 'Hide Details' : 'Show Details'}
+        </button>
+        {this.state.visibility && (
+          <div>
+            <p>Here are some details you can now see!</p>
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
-const renderApp = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={theGreatClick}>{buttonText}</button>
-      <p hidden={textVis}>Text that should be toggle-able</p>
-    </div>
-  )
-  ReactDOM.render(template, appRoot);
-}
-renderApp()
+
+//How I did it during the challenge.... a little different... but same function
+
+
+// class VisibilityToggle extends React.Component {
+//   constructor (props){
+//     super(props)
+//     this.handleToggleVisibility = this.handleToggleVisibility.bind(this)
+//     this.state = {
+//       visibility: true,
+//       buttonText: 'Show Details'
+//     }
+//   }
+//
+//   handleToggleVisibility() {
+//     this.setState((prevState) => {
+//       return {
+//         visibility: !prevState.visibility,
+//         buttonText: (prevState.buttonText == 'Show Details') ? prevState.buttonText = 'Hide Details' : prevState.buttonText = 'Show Details'
+//             }
+//     })
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Visibility Toggle</h1>
+//         <button onClick={this.handleToggleVisibility}>{this.state.buttonText}</button>
+//         <p hidden={this.state.visibility}>Text that should be toggle-able</p>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+ReactDOM.render(<VisibilityToggle />, appRoot)
