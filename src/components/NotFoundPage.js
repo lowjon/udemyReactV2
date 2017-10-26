36abc7c4667253b7903f79ebbfb574343d455d3e
Jon Low <lowjonc@gmail.com>
2017-10-26 12:26:56 -0600

    oops

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NotFoundPage = () => (
+  <div>
+    this is from the NotFoundPage
+  </div>
+)
+
+export default NotFoundPage
